9f21d5530886b98cfbeccc1bfb763b17d9911ab9
Svetlana <isaeva322@mail.ru>
2022-11-09 22:19:26 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Задача 1
+        System.out.println(" Задача 1");
+
+        Byte courses = 4;
+        System.out.println(" Значение переменной courses c типом byte = " + courses);
+        short semesters = 8;
+        System.out.println(" Значение переменной semesters c типом short = " + semesters);
+        int age = 25;
+        System.out.println(" Значение переменной age c типом int = " + age);
+        long year = 365L;
+        System.out.println(" Значение переменной year c типом long = " + year);
+        float x = 1.63f;
+        System.out.println(" Значение переменной x c типом float = " + x);
+        double numberForPi = 3.1415926535;
+        System.out.println(" Значение переменной number for Pi c типом double = " + numberForPi);
+
+
+
+
 
 
 
